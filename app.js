2ea3a32d84344fac71d672e76ef0fd6abcfaaa26
Suraez <oberais50@gmail.com>
2020-06-09 10:52:06 +0545

    changed app.js

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 // const config = require('config');
 
+
 const express = require('express');
 const app = express();
 
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+
+  // if (req.method === 'OPTIONS') {
+  //   res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+  //   return res.status(200).json({})
+  // }
+  next();
+})
 
 // routes
 const users = require('./routes/users');
